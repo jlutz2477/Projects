e9f541bccc8cb7b06807d90182d567df52c8d9e6
jlutz2477 <103705539+jlutz2477@users.noreply.github.com>
2023-01-02 22:40:31 -0500

    Update State.java

--- covid_tracker/Project_5-main/src/prj5/State.java
+++ covid_tracker/Project_5-main/src/prj5/State.java
@@ -1,11 +1,5 @@
 package prj5;
 
-// Virginia Tech Honor Code Pledge:
-// f
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Brenden Duffy bdeduffy
 
 /**
  * State Class, THis class represents a state with its corresponding
